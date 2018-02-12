c842eff8619e2ffed13f5be5fe74184f36115de9
Qiang <565057208@qq.com>
2018-02-12 10:30:26 +0800

    fix bugs

--- WXKit/WXKit.h
+++ WXKit/WXKit.h
@@ -5,8 +5,12 @@
 //  Created by 王家强 on 2018/1/31.
 //
 
+#ifdef __OBJC__
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#endif
 
+// import headers
 #import "WXKitMacro.h"
 #import "WXUtil.h"
 #import "WXCategroy.h"
